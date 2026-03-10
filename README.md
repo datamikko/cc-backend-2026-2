656cf3d38877e7bd7b18e3df202f1d37b55a5967
Mikko Pajula <mikko.pajula@lapinamk.fi>
2026-03-10 08:27:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Sentiment backend
+
+## Install
+
+Python version used: 3.11
+
+## Start app
+
+waitress-serve --host 127.0.0.1 main:app
+
+python app.py
---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import pickle
+
+app = Flask(__name__)
+
+with open("sentiment_model_v1.pkl", "rb") as file:
+    model = pickle.load(file)
+
+@app.route("/")
+def health():
+    return {"message": "alive"}
+
+@app.route("/api", methods=["POST"])
+def sentiment_api():
+    data = request.get_json()
+    print(data)
+    print(model.predict([data["sentiment"]]))
+    return {"sentence": data["sentiment"], "sentiment": "positive"}
